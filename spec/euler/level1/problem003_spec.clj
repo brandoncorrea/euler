1121461f8ccf9ba41d034fc5a423c5cc6d365346
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-10-01 18:55:17 -0400

    Solve Euler 3

--- spec/euler/level1/problem003_spec.clj
+++ spec/euler/level1/problem003_spec.clj
@@ -6,8 +6,13 @@
 ; https://projecteuler.net/problem=3
 
 (describe "Euler Problem #3"
-
   (it "Solves #3"
-      (should= 6857 (euler-3 600851475143)))
-  )
-
+    (should= 1 (euler-3 -1))
+    (should= 0 (euler-3 0))
+    (should= 1 (euler-3 1))
+    (should= 2 (euler-3 2))
+    (should= 13 (euler-3 13))
+    (should= 5 (euler-3 30))
+    (should= 37 (euler-3 111))
+    (should= 6857 (euler-3 -600851475143))
+    (should= 6857 (euler-3 600851475143))))
--- src/euler/level1/problem003.clj
+++ src/euler/level1/problem003.clj
@@ -1,42 +1,20 @@
 (ns euler.level1.problem003)
 
-; Returns true if the numerator is
-; evenly divisible by the denominator
-(defn divisible-by [numerator denominator]
-    (= 0 (mod numerator denominator)))
+(defn- divisible-by? [num div]
+  (= 0 (mod num div)))
 
-; Returns true if x is prime
-(defn prime? [x]
-  (every? false?
-    (map
-      #(divisible-by x %)
-      (vec (range 2 (inc (/ x 2)))))))
-
-; Returns true if divisor is a prime factor of num
-(defn prime-factor-of [divisor num]
-  (and
-    (divisible-by num divisor)
-    (prime? divisor)))
-
-; Returns true if num1 or num2 is greater than limit
-(defn limit-reached [limit num1 num2]
-  (or
-    (> num1 limit)
-    (> num2 limit)))
-
-; Returns the maximum prime factor of num
-(defn max-prime-factor-of [num]
-  (loop
-    [remainder num
-     factor 2
-     maxFactor num]
-    (cond
-      (limit-reached remainder factor maxFactor)
-        maxFactor
-      (prime-factor-of factor remainder)
-        (recur (/ remainder factor) factor factor)
-      :else
-        (recur remainder (inc factor) maxFactor))))
+(defn- abs [n]
+  (if (>= n 0)
+    n
+    (* -1 n)))
 
 (defn euler-3 [n]
-  (max-prime-factor-of n))
+  (loop [num (abs n)
+         div 2]
+    (cond
+      (>= div num)
+        num
+      (divisible-by? num div)
+        (recur (/ num div) div)
+      :else
+        (recur num (inc div)))))
