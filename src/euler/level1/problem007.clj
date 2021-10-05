830c63adcb96cbc15bbf37d2a50dd51a9d3e6e9f
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-10-05 17:09:38 -0400

    Refactor Problem 7

--- src/euler/level1/problem007.clj
+++ src/euler/level1/problem007.clj
@@ -1,13 +1,6 @@
 (ns euler.level1.problem007
-  (:use [euler.util.math :only [divisible-by?]]))
+  (:use [euler.util.math :only [divisible-by?]]
+        [euler.util.sequences :only [primes]]))
 
 (defn euler-7 [n]
-  (loop [num 2
-         primes []]
-    (cond
-      (>= (count primes) n)
-        (last primes)
-      (some #(divisible-by? num %) primes)
-        (recur (inc num) primes)
-      :else
-        (recur (inc num) (conj primes num)))))
+  (nth primes (dec n)))
