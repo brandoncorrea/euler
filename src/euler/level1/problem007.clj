79b2463fc0e3aa5914e051b95cf68e4be3e5d666
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-10-05 15:56:53 -0400

    Refactor Problem 7

--- src/euler/level1/problem007.clj
+++ src/euler/level1/problem007.clj
@@ -1,7 +1,5 @@
-(ns euler.level1.problem007)
-
-(defn- divisible-by? [num div]
-  (= 0 (mod num div)))
+(ns euler.level1.problem007
+  (:use [euler.util.math :only [divisible-by?]]))
 
 (defn euler-7 [n]
   (loop [num 2
