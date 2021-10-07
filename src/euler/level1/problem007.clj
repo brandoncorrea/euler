883df747e8094362a79a76c53a4c082e56c587cd
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-10-07 11:53:40 -0400

    Remove math import

--- src/euler/level1/problem007.clj
+++ src/euler/level1/problem007.clj
@@ -1,6 +1,5 @@
 (ns euler.level1.problem007
-  (:use [euler.util.math :only [divisible-by?]]
-        [euler.util.sequences :only [primes]]))
+  (:use [euler.util.sequences :only [primes]]))
 
 (defn euler-7 [n]
   (nth primes (dec n)))
