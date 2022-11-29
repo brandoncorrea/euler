bff08113d522f9b6962e6da6f2453c770661b125
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2022-11-29 11:32:54 -0500

    Update euler generator

--- ruby/euler.rb
+++ ruby/euler.rb
@@ -1,29 +1,40 @@
-problem_number = ARGV[0]
-problem_number_padded = problem_number.rjust(3, '0')
-
 def ensure_directory(path)
   Dir.mkdir(path) unless Dir.exist?(path)
 end
 
-ensure_directory('lib')
-ensure_directory('spec')
+def pad_3(n) n.rjust(3, '0') end
 
-lib_file = "lib/problem#{problem_number_padded}.rb"
-File.open(lib_file, 'w') unless File.exist?(lib_file)
+def create_lib_file(problem_number)
+  lib_file = "lib/problem#{pad_3(problem_number)}.rb"
+  unless File.exist?(lib_file)
+    File.open(lib_file, 'w') do |file|
+      file.write("def euler_#{problem_number}(n)\n  \nend\n")
+    end
+  end
+end
 
-spec_file = "spec/problem#{problem_number_padded}_spec.rb"
-unless File.exist?(spec_file)
-  File.open(spec_file, 'w') do |file|
-    file.write(
-"require 'problem#{problem_number_padded}'
+def create_spec_file(problem_number)
+  problem_number_padded = pad_3(problem_number)
+  spec_file = "spec/problem#{problem_number_padded}_spec.rb"
+  unless File.exist?(spec_file)
+    File.open(spec_file, 'w') do |file|
+      file.write(
+        "require 'problem#{problem_number_padded}'
 
-# http://projecteuler.net/index.php?section=problems&id=#{problem_number}
+# https://projecteuler.net/index.php?section=problems&id=#{problem_number}
 
 describe \"Euler Problem ##{problem_number}\" do
   it \"Solves #1\" do
-    expect(1).to eq 2
+    expect(euler_#{problem_number}(1)).to eq 1
   end
 end
 ")
+    end
   end
 end
+
+problem_number = ARGV[0]
+ensure_directory('lib')
+ensure_directory('spec')
+create_lib_file(problem_number)
+create_spec_file(problem_number)
